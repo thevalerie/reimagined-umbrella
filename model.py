56c3950b829a34b3b8e53fe239829da69875b077
Valerie Moy <thevalerie@gmail.com>
2018-04-23 21:31:14 -0700

    SQLAlchemy track modifications throwing error

--- model.py
+++ model.py
@@ -29,7 +29,7 @@ def connect_to_db(app):
 
     # Configure to use our PostgreSQL database
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.app = app
     db.init_app(app)
 

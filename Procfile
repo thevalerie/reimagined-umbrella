679f3344dc309c0c1d7b22b06a0a6b70ff180a12
Valerie Moy <thevalerie@gmail.com>
2018-04-23 20:50:05 -0700

    More Heroku debugging

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn model:app
+web: gunicorn server:app
--- server.py
+++ server.py
@@ -33,13 +33,13 @@ def add_new_message():
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the
     # point that we invoke the DebugToolbarExtension
-    app.debug = True
+    # app.debug = True
     # make sure templates, etc. are not cached in debug mode
-    app.jinja_env.auto_reload = app.debug
+    # app.jinja_env.auto_reload = app.debug
 
-    connect_to_db(app)
+    # connect_to_db(app)
 
     # Use the DebugToolbar
     # DebugToolbarExtension(app)
 
-    app.run(port=5000, host='0.0.0.0')
+    app.run()

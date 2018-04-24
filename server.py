7fc73c732624c7286fe19b340210fca4786e1a21
Valerie Moy <thevalerie@gmail.com>
2018-04-23 21:20:05 -0700

    Fixed issue with connecting db

--- server.py
+++ server.py
@@ -37,7 +37,7 @@ if __name__ == "__main__":
     # make sure templates, etc. are not cached in debug mode
     # app.jinja_env.auto_reload = app.debug
 
-    # connect_to_db(app)
+    connect_to_db(app)
 
     # Use the DebugToolbar
     # DebugToolbarExtension(app)
